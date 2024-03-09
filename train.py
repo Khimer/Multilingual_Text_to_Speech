a110fd733ee0b93dfd198b8460db85cd1eca5330
Khimer <zenigeba77@gmail.com>
2024-03-09 19:28:14 +0700

    tqdm

--- train.py
+++ train.py
@@ -47,7 +47,7 @@ def train(logging_start_epoch, epoch, data, model, criterion, optimizer):
     done, start_time = 0, time.time()
 
     # loop through epoch batches
-    for i, batch in tqdm(enumerate(data)):     
+    for i, batch in tqdm(enumerate(data), total=len(data)):
 
         global_step = done + epoch * len(data)
         optimizer.zero_grad() 
